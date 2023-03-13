cab62145e1cd715cc40a7d08253be0b115891400
WesT <teixeiraw42@hotmail.com>
2023-03-13 09:16:38 -0300

    Merge with main repository

--- target_snowflake/file_formats/csv.py
+++ target_snowflake/file_formats/csv.py
@@ -6,7 +6,7 @@ import os
 from typing import Callable, Dict, List
 from tempfile import mkstemp
 
-import target_snowflake.flattening as flattening
+from target_snowflake import flattening
 
 
 def create_copy_sql(table_name: str,
@@ -37,24 +37,20 @@ def create_merge_sql(table_name: str,
                      columns: List,
                      pk_merge_condition: str) -> str:
     """Generate a CSV compatible snowflake MERGE INTO command"""
-    return "MERGE INTO {} t USING (" \
-           "SELECT {} " \
-           "FROM '@{}/{}' " \
-           "(FILE_FORMAT => '{}')) s " \
-           "ON {} " \
-           "WHEN MATCHED THEN UPDATE SET {} " \
+    p_source_columns = ', '.join([f"{c['trans']}(${i + 1}) {c['name']}" for i, c in enumerate(columns)])
+    p_update = ', '.join([f"{c['name']}=s.{c['name']}" for c in columns])
+    p_insert_cols = ', '.join([c['name'] for c in columns])
+    p_insert_values = ', '.join([f"s.{c['name']}" for c in columns])
+
+    return f"MERGE INTO {table_name} t USING (" \
+           f"SELECT {p_source_columns} " \
+           f"FROM '@{stage_name}/{s3_key}' " \
+           f"(FILE_FORMAT => '{file_format_name}')) s " \
+           f"ON {pk_merge_condition} " \
+           f"WHEN MATCHED THEN UPDATE SET {p_update} " \
            "WHEN NOT MATCHED THEN " \
-           "INSERT ({}) " \
-           "VALUES ({})".format(
-        table_name,
-        ', '.join(["{}(${}) {}".format(c['trans'], i + 1, c['name']) for i, c in enumerate(columns)]),
-        stage_name,
-        s3_key,
-        file_format_name,
-        pk_merge_condition,
-        ', '.join(['{0}=s.{0}'.format(c['name']) for c in columns]),
-        ', '.join([c['name'] for c in columns]),
-        ', '.join(['s.{}'.format(c['name']) for c in columns]))
+           f"INSERT ({p_insert_cols}) " \
+           f"VALUES ({p_insert_values})"
 
 
 def record_to_csv_line(record: dict,
